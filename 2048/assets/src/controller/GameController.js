3854388deedfd6034e93692c8c2e7b47968aa549
huadududu <huaduber@163.com>
2018-07-06 22:23:17 +0800

    save

--- 2048/assets/src/controller/GameController.js
+++ 2048/assets/src/controller/GameController.js
@@ -12,6 +12,7 @@ let Global = require("Global");
 let GameState = require("GameState");
 let BingLog = require("BingLog");
 let GameUtils = require("GameUtils");
+let GameConfig = require("GameConfig");
 cc.Class({
     extends: cc.Component,
 
@@ -23,12 +24,12 @@ cc.Class({
         //     type: cc.SpriteFrame, // optional, default is typeof default
         //     serializable: true,   // optional, default is true
         // },
-        BlocksController:require("BlocksControllers"),
-        GameMenuController:require("GameMenuController"),
-        startMenu:cc.Node,
-        endMenu:cc.Node,
-        continueMenu:cc.Node,
-        loadingad:cc.Node,
+        BlocksController: require("BlocksControllers"),
+        GameMenuController: require("GameMenuController"),
+        startMenu: cc.Node,
+        endMenu: cc.Node,
+        continueMenu: cc.Node,
+        loadingad: cc.Node,
 
     },
 
@@ -40,54 +41,56 @@ cc.Class({
     },
 
 
-
-
     start() {
         // this.createBlocks();
         // this.schedule(this.refreshBlocks, 0.5);
     },
-    updeteFinish:function(){
+    updeteFinish: function () {
         this.endMenu.active = true;
         Global.newHistory(Global.thisscore);
     },
     //create block;
-    onTouchStartBtn:function(){
+    onTouchStartBtn: function () {
         this.startMenu.active = false;
         this.BlocksController.restartMenu();
 
     },
-    onTouchRestartBtn:function(){
+    onTouchRestartBtn: function () {
         this.endMenu.active = false;
-        let num = GameUtils.randomInt(0,100);
-        if(num < 30){
-            let FBP = require("Plugin");
-            this.loadingad.active = true;
-            FBP.RewardedVideoAsync(this.restartCallBack.bind(this));
-        }else if(num < 80){
-            let FBP = require("Plugin");
-            this.loadingad.active = true;
-            FBP.InterstitialAdAsync(this.restartCallBack.bind(this));
-        }else{
+        if (GameConfig.isFBInstantGame()) {
+            let num = GameUtils.randomInt(0, 100);
+            if (num < 30) {
+                let FBP = require("Plugin");
+                this.loadingad.active = true;
+                FBP.RewardedVideoAsync(this.restartCallBack.bind(this));
+            } else if (num < 80) {
+                let FBP = require("Plugin");
+                this.loadingad.active = true;
+                FBP.InterstitialAdAsync(this.restartCallBack.bind(this));
+            } else {
+                this.BlocksController.restartMenu();
+            }
+        } else {
             this.BlocksController.restartMenu();
         }
 
     },
 
-    onTouchShareBtn:function(){
+    onTouchShareBtn: function () {
         let FBP = require("Plugin");
         FBP.chooseAsync();
 
     },
-    onTouchPause:function(){
+    onTouchPause: function () {
         this.continueMenu.active = true;
         cc.director.pause();
     },
 
-    onTouchResume:function(){
+    onTouchResume: function () {
         this.continueMenu.active = false;
         cc.director.resume();
     },
-    restartCallBack:function(){
+    restartCallBack: function () {
         this.loadingad.active = false;
         this.BlocksController.restartMenu();
     },
